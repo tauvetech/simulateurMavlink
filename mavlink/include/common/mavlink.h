1b4514a8982aaa7a7507b5f412dbd23325ed41ac
tauvetech <tauvetech@gmail.com>
2012-06-05 22:40:47 +0200

    ajout du protocole Mavlink de test.

---
+++ mavlink/include/common/mavlink.h
@@ -0,0 +1,11 @@
+/** @file
+ *	@brief MAVLink comm protocol.
+ *	@see http://pixhawk.ethz.ch/software/mavlink
+ *	 Generated on Friday, January 7 2011, 10:04 UTC
+ */
+#ifndef MAVLINK_H
+#define MAVLINK_H
+
+#include "common.h"
+
+#endif
